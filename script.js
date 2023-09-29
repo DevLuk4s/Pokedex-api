116f20c9ae13f4125d5a35da9b6fd54b314e3faf
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-09-29 00:16:48 -0300

    atualizado

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const input = document.querySelector(".input__search")
 const buttonNext = document.querySelector(".btn-next")
 const buttonPrev = document.querySelector(".btn-prev")
 
-let pokemon = 1
+let Searchpokemon = 1
 
 const fetchPokemon = async (pokemon) => {
     const respose = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -17,11 +17,38 @@ const fetchPokemon = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon)
-    console.log(data)
 
-    PokemonName.innerHTML = data.name
-    PokemonNumber.innerHTML = data.id
-    PokemonNumber.src = data.sprite.front_default
+    if(data) {
+        PokemonImage.style.display = 'block';
+        PokemonName.innerHTML = 'Loanding...';
+        PokemonNumber.innerHTML = '';
+        PokemonName.innerHTML = data.name
+        PokemonNumber.innerHTML = data.id
+        PokemonImage.src = data.sprites.versions["generation-v"]["black-white"].animated.front_default
+        input.value = '';
+        Searchpokemon = data.id;
+    } else {
+        PokemonImage.style.display = 'none';
+        PokemonName.innerHTML = 'Not found :c';
+        PokemonNumber.innerHTML = '';
+    }
 }
 
-renderPokemon(pokemon)
+form.addEventListener("submit",(evt) => {
+    evt.preventDefault()
+    renderPokemon(input.value.toLocaleLowerCase())
+})
+
+buttonNext.addEventListener("click",() => {
+    Searchpokemon += 1
+    renderPokemon(Searchpokemon)
+})
+
+buttonPrev.addEventListener("click",() => {
+    if(Searchpokemon > 1) {
+        Searchpokemon -= 1
+        renderPokemon(Searchpokemon)
+    }
+})
+
+renderPokemon(Searchpokemon)
