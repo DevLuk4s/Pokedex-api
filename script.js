508898a978f3bac634aa61fe4e9c3c82526c4d13
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-09-28 22:58:14 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const pokemonName = document.querySelector('.pokemon__name')
+const pokemonImg = document.querySelector('.pokemon__image')
+const pokemonId = document.querySelector('.pokemon__number')
 
 const fechPokemon = async (pokemon) => {
     const respose = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -11,6 +13,8 @@ const renderPokemon = async (pokemon) => {
     const data = await fechPokemon(pokemon)
     
     pokemonName.innerHTML = data.name
+    pokemonId.innerHTML = data.id
+    pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 }
 
-renderPokemon('25')
+renderPokemon(25)
